9d2fd112c675e9e7179ec2d992e64097df70f54f
mi-gand <m.gandrabura@mail.ru>
2025-04-16 12:36:20 +0300

    refactor: removed static index.html and added root endpoint handler

--- src/main/java/org/example/controller/VacationPayController.java
+++ src/main/java/org/example/controller/VacationPayController.java
@@ -19,13 +19,19 @@ public class VacationPayController {
 
     private final VacationPayService vacationPayService;
 
+    @GetMapping("/")
+    public String start(){
+        log.debug("start Page");
+        return "startPage";
+    }
+
     @GetMapping("/calculate")
     public String calculate(@ModelAttribute VacationRequestDTO dtoFromPage, Model model) {
         log.debug("DTO from page: {}", dtoFromPage);
         String validationError = validateRequest(dtoFromPage);
         if (validationError != null) {
             model.addAttribute("alertMessage", validationError);
-            return "afterBadValidation";
+            return "startPage";
         }
         VacationResponseDTO dtoToPage = vacationPayService.createResponseDTO(dtoFromPage);
         model.addAttribute("VacationResultDTO", dtoToPage);
